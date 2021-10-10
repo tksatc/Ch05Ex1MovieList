8840a7ed6e22ed1a5211e063440b31a77b3fa683
tksatc <tksatc@gmail.com>
2021-10-09 17:16:24 -0800

    Done

--- MovieList/Startup.cs
+++ MovieList/Startup.cs
@@ -15,7 +15,7 @@ namespace MovieList
 
         public IConfiguration Configuration { get; }
 
-        // Use this method to add services to the container.
+        // Use this method to add services to the container...
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRouting(options => {
